c94805f567bef9e44b76dcab43e24c84775322ad
surajverma09 <surajverma6273@gmail.com>
2026-08-15 12:00:53 +0530

    Add Python Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/11. Container With Most Water/Solution.py
+++ LeetCode/Patterns/Two Pointers/11. Container With Most Water/Solution.py
@@ -6,18 +6,17 @@ class Solution:
 
         while left < right:
             width = right - left
-            if height[left] <= height[right]:
-                hight = height[left]
-            else:
-                hight = height[right]
+
+            hight = min(height[left], height[right])     
 
             maxi_water = width * hight
-            if max_water < maxi_water:
-                max_water = maxi_water
+            
+            max_water = max(maxi_water, max_water)
             
             if height[left] > height[right]:
                 right = right - 1
             else:
                 left = left + 1
+            
         
         return max_water
