1f9995f2e86e5630397c5a76b91711168a18b77c
surajverma09 <surajverma6273@gmail.com>
2026-08-15 11:59:46 +0530

    Add Python Solution to Patterns/Stack & Queue

---
+++ LeetCode/Patterns/Stack & Queue/1128. Remove All Adjacent Duplicates In String/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+
+        for i in s:
+            if len(stack)==0:
+                stack.append(i)
+            else:
+                if i == stack[-1]:
+                    stack.pop()
+                else:
+                    stack.append(i)
+        return "".join(stack)
+
